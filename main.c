49a9aa8e6506d62aeb7df3064c6a08935ccf4084
snandip9 <siri.nandipaty2@mail.dcu.ie>
2026-07-15 20:43:27 +0100

    added structs but have not used them yet, added cursor movement, commiting because I will be refactorign everything soon

--- main.c
+++ main.c
@@ -12,13 +12,20 @@
 
 
 int main(int argc, char ** argv)
+
+	//initialising variables 
 {	
 	FILE *src;
-	char s[500];
+	char s[500]; // text from the file
+	int row, col;
+
 	int cursorX = 5;
 	int cursorY = 3;
 	int ch;
+
 	WINDOW *my_win;
+
+	// checking if the file exists 
 
 	if (argc != 2) {
 		printf("Filename missing!\n");
@@ -30,16 +37,26 @@ int main(int argc, char ** argv)
 		perror("fopen");
 		exit(EXIT_FAILURE);}
 
+	// initialising screen and window and the settings for them 
+
 	initscr();
+	getmaxyx(stdscr,row,col);	
 	cbreak();
 	noecho();
-	my_win=newwin(100, 100, 0, 0);
+	keypad(stdscr, TRUE);
+	raw();	
+	my_win=newwin(row, col, 0, 0);
+
+	// getting the text from the file and presenting it on the screen 
 
 	fgets(s, sizeof(s), src);
 	mvwprintw(my_win, 3, 5, "%s", s);
 	wrefresh(my_win);
 
 	keypad(my_win, TRUE);
+
+
+	// where all commands are performed 
 
 
 	while((ch = wgetch(my_win)) != CTRL_KEY('q'))
@@ -60,6 +77,16 @@ int main(int argc, char ** argv)
 			    cursorY++;
 			    break;
 		}
+
+		if (cursorX < 0)
+		    cursorX = 0;
+		if (cursorY < 0)
+		    cursorY = 0;
+
+		if (cursorX > getmaxx(my_win) - 1)
+		    cursorX = getmaxx(my_win) - 1;
+		if (cursorY > getmaxy(my_win) - 1)
+		    cursorY = getmaxy(my_win) - 1;
 
 		wmove(my_win, cursorY, cursorX);
 		wrefresh(my_win);
